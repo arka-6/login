d47f520d6f0bc50d5bce4e47192b31d07658b9f1
ARKA ADHIKARY <arka.nghss@gmail.com>
2023-01-08 19:36:31 +0530

    changes

---
+++ src/public/js/app.js
@@ -0,0 +1,37 @@
+document.getElementById("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  validateForm();
+});
+
+function validateForm() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  if (username == "" || password == "") {
+    alert("Please enter a username and password");
+  } else if (password.length < 8) {
+    alert("Password must be at least 8 characters long");
+  } else if (!/^[a-zA-Z0-9_]+$/.test(username)) {
+    alert("Username can only contain letters, numbers, and underscores");
+  } else {
+    document.getElementById("form").submit();
+    console.log("Login successful");
+  }
+}
+
+function toggleLoginForm(isRegister) {
+  loginForm = document.getElementById("loginForm");
+  loginForm.reset();
+  let btnToggler = document.getElementById("btn-toggle");
+  if (isRegister) {
+    btnToggler.classList.add("btn-toggler-second");
+    loginForm.children[5].classList.add("hide"); 
+  } else {
+    btnToggler.classList.remove("btn-toggler-second");
+    loginForm.children[5].classList.remove("hide");
+}
+}
+  /*Changes:-
+  1. login to register button submission
+  2. name and i agree to terms & conditions remove
+  */
