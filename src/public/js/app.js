f622122a538d542d41a4d1d002054b261aeacb86
ARKA ADHIKARY <arka.nghss@gmail.com>
2023-01-10 23:10:13 +0530

    changes

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -19,19 +19,61 @@ function validateForm() {
   }
 }
 
+
 function toggleLoginForm(isRegister) {
+  loginForm = document.getElementById("loginForm");
+  loginForm.reset();
+  let btnToggler = document.getElementById("btn-toggle");
+  
+  let loginSubmitBtn = document.getElementById("loginSubmitBtn");
+  
+  let registerSubmitBtn = document.getElementById("registerSubmitBtn");
+  if (!registerSubmitBtn) {
+    registerSubmitBtn = document.createElement("button");
+    registerSubmitBtn.innerText = "REGISTER";
+    registerSubmitBtn.id = "registerSubmitBtn";
+    registerSubmitBtn.classList.add("hide");
+    loginForm.appendChild(registerSubmitBtn);
+  }
+  
+  if (isRegister) {
+    btnToggler.classList.add("btn-toggler-second");
+    loginForm.children[5].classList.add("hide");
+    loginForm.children[0].classList.remove("hide");
+    loginForm.children[3].classList.remove("hide");
+    loginSubmitBtn.classList.add("hide");
+    registerSubmitBtn.classList.remove("hide");
+  } else {
+    btnToggler.classList.remove("btn-toggler-second");
+    loginForm.children[5].classList.remove("hide");
+    loginForm.children[0].classList.add("hide");
+    loginForm.children[3].classList.add("hide");
+    registerSubmitBtn.classList.add("hide");
+    loginSubmitBtn.classList.remove("hide");
+  }
+}
+
+/*function toggleLoginForm(isRegister) {
   loginForm = document.getElementById("loginForm");
   loginForm.reset();
   let btnToggler = document.getElementById("btn-toggle");
   if (isRegister) {
     btnToggler.classList.add("btn-toggler-second");
-    loginForm.children[5].classList.add("hide"); 
+    loginForm.children[5].classList.add("hide");
+    loginForm.children[0].classList.remove("hide");
+    loginForm.children[3].classList.remove("hide");
+    
+    
   } else {
     btnToggler.classList.remove("btn-toggler-second");
     loginForm.children[5].classList.remove("hide");
-}
-}
-  /*Changes:-
+    loginForm.children[0].classList.add("hide");
+    loginForm.children[3].classList.add("hide");
+    
+  }
+}*/
+
+/*Changes:-
   1. login to register button submission
   2. name and i agree to terms & conditions remove
   */
